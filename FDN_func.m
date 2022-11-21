be7bcf9ef94d6f88b62d62711a991eed45f55c8d
Marcus <marcus.hattula@aalto.fi>
2022-11-21 15:38:53 +0200

    Added impulse response and modified initial values.

--- FDN_func.m
+++ FDN_func.m
@@ -27,14 +27,14 @@ function echo_audio = FDN_func(wav, A, b, c, g, delays)
     sz_g = length(g);
     sz_delays = length(delays);
     assert(sz_A_1 == sz_A_2 && sz_A_2 == sz_b && sz_b == sz_c && sz_c == sz_g && sz_g == sz_delays, 'Array lengths must be equal');
+    
     N = sz_b;
     L = length(wav);
     max_delay = max(delays); % reverbation sound takes longer time than 
     
-    %A = A/max(A, [], 'all');        % normalized feedback matrix 
+    A = A/max(A, [], 'all');        % normalized feedback matrix
 
     X = zeros(L+max_delay,N);         % initialize delay lines
-    X(1,:) = b*wav(1);        % initialize first elements of delay line starting at max index
 
     for j = 1:L
         for i = 1:N
@@ -45,5 +45,5 @@ function echo_audio = FDN_func(wav, A, b, c, g, delays)
         end
     end
 
-    echo_audio = sum(c.*X, 2);
+    echo_audio = sum(c.*X, 2)/N;
 end
--- project.m
+++ project.m
@@ -1,22 +1,31 @@
 clear all; close all;
 
 [wav, fs] = audioread('testTrack.wav');
+unitimpulse = zeros(1e5,1);
+unitimpulse(1) = 1;
 
-N = 16;                             % number of interconnected delay lines
-A = eye(N);                         % feedback matrix equals to identity matrix
-T60 = 2;                            % Reverberation time -- higher value brings more echo ]1, 4[, for best results
+
+N = 128;                                % number of interconnected delay lines
+A = randn(N);                           % feedback matrix
+T60 = 0.5;                              % reverberation time
 g_dB = -60/(T60 * fs);
 g_lin = power(10, (g_dB/20));
 
-b_gains = rand(N,1);                % Randomised values between [0, 1] for input gains b_gains
-c_gains = rand(1, N);               % Randomised values between [0, 1] for output gains c_gains
-g_gains = ones(1, N);               % Setting all g_gain values to one
-m_delays = randi([500,2000],[1,N]); % For randomised delay values between [500, 2000]
-
-for i=1:16                          % g_gains calculated by given equation
-    g_gains(i) = power(g_lin, m_delays(i));
-end
+b_gains = rand(1, N);        % Randomised values between [0, 1] for input gains b_gains
+c_gains = rand(1, N);        % Randomised values between [0, 1] for output gains c_gains
+m_delays = randi([2000, 10000],[1,N]);   % For randomised delay values between [2000, 10000]
+g_gains = power(g_lin, m_delays);       % g_gains calculated by given equation
 
 y = FDN_func(wav, A, b_gains, c_gains, g_gains, m_delays);
+impulse_response = FDN_func(unitimpulse, A, b_gains, c_gains, g_gains, m_delays);
 
 soundsc(y, fs)
+
+L = length(impulse_response);
+stem(linspace(1, L, L), 10*log10(abs(impulse_response)), '|', 'BaseValue', -Inf)
+grid on
+xlim([-1e4, L])
+ylim([-Inf, 10])
+xlabel('Samples')
+ylabel('Amplitude [dB]')
+title('FDN system impulse response')
