24748b49e79f4422d2411659cd21c7d308f869bc
Marcus <marcus.hattula@aalto.fi>
2022-11-21 16:19:00 +0200

    Better impulse response.

--- project.m
+++ project.m
@@ -1,27 +1,30 @@
 clear all; close all;
 
 [wav, fs] = audioread('testTrack.wav');
+unitimpulse = zeros(1e5,1);
+unitimpulse(1) = 1;
 
-N = 16;                             % number of interconnected delay lines
-A = eye(N);                         % feedback matrix equals to identity matrix
-T60 = 3;                          % Reverberation time -- higher value brings more echo
-% closet 0.3
-% normal room 0.7
-% hall 1.5
-% crazy eco 3
-
+N = 128;                                % number of interconnected delay lines
+A = randn(N);                           % feedback matrix
+T60 = 0.6;                              % reverberation time
 g_dB = -60/(T60 * fs);
 g_lin = power(10, (g_dB/20));
 
-b_gains = rand(N,1);                % Randomised values between [0, 1] for input gains b_gains
-c_gains = rand(1, N);               % Randomised values between [0, 1] for output gains c_gains
-g_gains = ones(1, N);               % Setting all g_gain values to one
-m_delays = randi([500,2500],[1,N]); %1000*primes(54);        % For randomised delay values between [500, 2000]
-
-for i=1:16                          % g_gains calculated by given equation
-    g_gains(i) = power(g_lin, m_delays(i));
-end
+b_gains = rand(1, N);        % Randomised values between [0, 1] for input gains b_gains
+c_gains = rand(1, N);        % Randomised values between [0, 1] for output gains c_gains
+m_delays = randi([2000, 10000],[1,N]);   % For randomised delay values between [2000, 10000]
+g_gains = power(g_lin, m_delays);       % g_gains calculated by given equation
 
 y = FDN_func(wav, A, b_gains, c_gains, g_gains, m_delays);
+impulse_response = FDN_func(unitimpulse, A, b_gains, c_gains, g_gains, m_delays);
 
 soundsc(y, fs)
+
+L = length(impulse_response);
+stem(linspace(1, L, L), 10*log10(abs(impulse_response)), '|', 'BaseValue', -Inf)
+grid on
+xlim([-1e4, L])
+ylim([-Inf, 10])
+xlabel('Samples')
+ylabel('Amplitude [dB]')
+title('FDN system impulse response')
